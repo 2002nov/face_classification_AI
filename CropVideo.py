6f8a022d61c4506f8ed8fcf8bc325909183937ea
Pawita P <124026666+2002nov@users.noreply.github.com>
2024-05-29 14:22:14 +0700

    Add files via upload

--- CropVideo.py
+++ CropVideo.py
@@ -2,11 +2,13 @@ import cv2 as cv
 from ultralytics import YOLO
 import pathlib
 import os
+from PIL import Image
+import numpy as np
 
 # Load YOLOv8 model
 detect_model = YOLO('/media/lab_brownien/data1/Work_Student2024_V2/AI_train/Tang/yolov8l-face.pt')
 
-classify_model = '/media/lab_brownien/data1/Work_Student2024_V2/AI_train/Tang/runs/classify/train15#/weights/best.pt'
+classify_model = YOLO('/media/lab_brownien/data1/Work_Student2024_V2/AI_train/Tang/runs/classify/train15#/weights/best.pt')
 class_dict = {0: 'Angry', 1: 'Bored', 2: 'Confused', 3: 'Cool', 4: 'Errrr', 5: 'Funny', 6: 'Happy', 7: 'Normal', 8: 'Proud', 9: 'Sad', 10: 'Scared', 11: 'Shy', 12: 'Sigh', 13: 'Superangry', 14: 'Surprised', 15: 'Suspicious', 16: 'Unhappy', 17: 'Worried', 18: 'sweet', 19: 'tricky'}
 
 class_predict = []
@@ -22,6 +24,7 @@ frame_counter = 0
 
 while cap.isOpened():
     face_counter = 0  # Counter for detected faces
+    faces = []
 
     ret, frame = cap.read()
     
@@ -45,22 +48,25 @@ while cap.isOpened():
             cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv.putText(frame, label, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             
-            # Extract and save the detected face
+            # Extract the detected face
             face = frame[y1:y2, x1:x2]
-            face_path = os.path.join(output_dir, f"{frame_counter}_face_{face_counter}.jpg")
-            cv.imwrite(face_path, face)
+            #face_path = os.path.join(output_dir, f"{frame_counter}_face_{face_counter}.jpg")
+            #cv.imwrite(face_path, face)
             face_counter += 1
+            faces.append(face)
 
-    files = [f for f in pathlib.Path('/media/lab_brownien/data1/Work_Student2024_V2/AI_train/Tang/detected_faces').glob(f"{frame_counter}_face_*.jpg")]
-    files.sort()
-    for i in files:
-    	path = str(i)
-    	results = classify_model.predict(path)  # Use classify_model instead of model
-    	class_predict.append(class_dict[results[0].probs.data.argmax().item()])
+            # Classify the face
+            predicts = classify_model.predict(face)
+            prediction_idx = class_dict[predicts[0].probs.data.argmax().item()]
+            print('frame'+str(frame_counter)+' face'+str(face_counter)+' : '+prediction_idx)
+            #prediction = class_dict[prediction_idx]
 
-    # Print the predicted emotions for each detected face
-    for emotion in class_predict:
-    	print(emotion)
+    # put all prediction in array then print at once
+    #for i in faces:
+        #predicts = classify_model.predict(face_image)
+        #prediction_idx = predicts[0].probs.data.argmax().item()
+        #class_predict.append(prediction_idx)
+    #print(class_predict)
 
     # Display the frame with detected objects
     cv.imshow('frame', frame)
